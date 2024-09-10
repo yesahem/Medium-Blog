2a9696b89a16d431c3f0d08af4f95b1a30850c9d
yesahem <ahemraj82@g>
2024-09-10 23:02:47 +0530

    commom

--- common/src/validations.ts
+++ common/src/validations.ts
@@ -1,6 +1,6 @@
-// These are the validation that is going to be pushed over to npm 
+// These are the validation that is going to be pushed over to npm
 
-// only ".d.ts" is going to be pushed and not these file 
+// only ".d.ts" is going to be pushed and not these file
 
 import { z } from "zod";
 
@@ -9,25 +9,25 @@ console.log(z);
 const signupSchema = z.object({
   name: z.string(),
   email: z.string().email(),
-  password: z.string().min(8),
+  password: z.string().min(8, "Password Must contains atleast 8 characters"),
 });
 
 const signinSchema = z.object({
   email: z.string().email(),
 });
- const blogSchema = z.object({
-    title: z.string(),
-    content: z.string(),
-    published: z.boolean(),
-  });
-  
-   const updateBlogSchema = z.object({
-    id: z.string(),
-    title: z.string(),
-    content: z.string(),
-    published: z.boolean(),
-  });
-  
+const blogSchema = z.object({
+  title: z.string(),
+  content: z.string(),
+  published: z.boolean(),
+});
+
+const updateBlogSchema = z.object({
+  id: z.string(),
+  title: z.string(),
+  content: z.string(),
+  published: z.boolean(),
+});
+
 export type SignupSchema = z.infer<typeof signupSchema>;
 export type SigninSchema = z.infer<typeof signinSchema>;
 export type BlogSchema = z.infer<typeof blogSchema>;
