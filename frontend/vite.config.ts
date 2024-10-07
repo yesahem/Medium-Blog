4a2786a50d735bc7953f21adcd8f400ee831d2db
Ashutosh Pandey <pandeyashu008@gmail.com>
2024-10-07 18:39:49 +0530

    feat: add docker and docker compose

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,31 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   optimizeDeps: {
+//     exclude: ["bcrypt"],
+//   },
+// });
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-    exclude: ["bcrypt"],
-  },
+ base: "/",
+ plugins: [react()],
+ optimizeDeps: {
+  exclude: ["bcrypt"],
+ },
+ preview: {
+  port: 3000,
+  strictPort: true,
+ },
+ server: {
+  port: 3000,
+  strictPort: true,
+  host: true,
+  origin: "http://0.0.0.0:3000",
+ },
 });
