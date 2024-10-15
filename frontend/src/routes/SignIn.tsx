44f771daa3d1f081cf1ca3634833f065a077c3ff
Abhii <abhishekhsharma2002@gmail.com>
2024-10-15 13:11:40 +0530

    auth page redesign

--- frontend/src/routes/SignIn.tsx
+++ frontend/src/routes/SignIn.tsx
@@ -46,8 +46,7 @@ export default function SignIn() {
         } else {
           alertError("Incorrect credentials");
         }
-      }
-    )
+      });
   }
 
   return (
@@ -57,7 +56,89 @@ export default function SignIn() {
           <DarkModeToggle />
         </div>
       </header>
-      <section className="w-full py-12 md:py-24 lg:py-32">
+      <section className="w-full flex h-[calc(100vh-80px)] px-10 sm:px-20 py-8">
+        <div className="w-full lg:w-1/2 h-full flex justify-center items-center flex-col lg:block relative">
+          <div className="w-full text-gray-900 dark:text-gray-100">
+            <div>
+              <h1 className="text-xl font-medium sm:text-3xl ">
+                Welcome back!
+              </h1>
+              <p className="text-sm opacity-55 py-2">
+                Please login to your account
+              </p>
+            </div>
+
+            <div className="py-6">
+              <div className="">
+                <label
+                  htmlFor="email"
+                  className="block text-sm font-medium text-gray-900 dark:text-gray-100 py-2"
+                >
+                  Email
+                </label>
+                <input
+                  id="email"
+                  type="email"
+                  placeholder="yourmail@example.com"
+                  required
+                  onChange={(e) => {
+                    setEmail(e.target.value);
+                  }}
+                  className="block  w-full lg:w-2/3 p-2 border rounded bg-gray-50 dark:bg-gray-700 text-gray-900 dark:text-gray-100 border-gray-300 dark:border-gray-600 focus:ring-blue-500 focus:border-blue-500"
+                />
+              </div>
+            </div>
+            <div className="space-y-2">
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium text-gray-900 dark:text-gray-100"
+              >
+                Password
+              </label>
+              <input
+                id="password"
+                type="password"
+                required
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
+                className="block w-full lg:w-2/3 p-2 border rounded bg-gray-50 dark:bg-gray-700 text-gray-900 dark:text-gray-100 border-gray-300 dark:border-gray-600 focus:ring-blue-500 focus:border-blue-500"
+                placeholder="Your password"
+              />
+            </div>
+            <div className="pt-10">
+              <button
+                className="w-full lg:w-2/3 p-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition-colors duration-200"
+                onClick={signinHandler}
+              >
+                Sign in
+              </button>
+            </div>
+
+            <div className="flex justify-center items-center w-full lg:w-2/3 pt-20 text-sm text-gray-600 dark:text-gray-400">
+              Don&apos;t have an account?{" "}
+              <Link
+                to="/signup"
+                className="underline text-blue-500 hover:text-blue-600"
+              >
+                Sign up
+              </Link>
+            </div>
+            <div className="absolute bottom-0 left-0 w-full lg:w-2/3  flex justify-center items-center">
+              <div className="">
+                <p className="italic">
+                  "Be yourself; everyone else is already taken."
+                </p>
+                <p className="text-center italic text-sm">― Oscar Wilde </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="hidden h-full lg:block w-full sm:w-1/2">
+          <img src="./writer.svg" alt="writer" className="" />
+        </div>
+      </section>
+      {/* <section className="w-full py-12 md:py-24 lg:py-32">
         <div className="container px-4 md:px-6 grid gap-6 lg:grid-cols-2 lg:gap-12">
           <div className="flex flex-col justify-center space-y-4">
             <div className="space-y-2">
@@ -134,7 +215,7 @@ export default function SignIn() {
             </div>
           </div>
         </div>
-      </section>
+      </section> */}
     </main>
   );
 }
--- frontend/src/routes/Signup.tsx
+++ frontend/src/routes/Signup.tsx
@@ -69,7 +69,109 @@ export default function SignUp() {
           <DarkModeToggle />
         </div>
       </header>
-      <section className="w-full py-12 md:py-24 lg:py-32">
+      <section className="w-full flex h-[calc(100vh-80px)] px-10 sm:px-20 py-8">
+        <div className="w-full lg:w-1/2 h-full flex justify-center items-center flex-col lg:block relative">
+          <div className="w-full text-gray-900 dark:text-gray-100">
+            <div>
+              <h1 className="text-xl font-medium sm:text-3xl ">
+                Create a new account!
+              </h1>
+              <p className="text-sm opacity-55 py-2">
+                {" "}
+                Enter your details to create a new account
+              </p>
+            </div>
+
+            <form className="space-y-4 w-full lg:w-2/3 py-6">
+              <div className="space-y-2">
+                <label
+                  htmlFor="name"
+                  className="block text-sm font-medium text-gray-900 dark:text-gray-100"
+                >
+                  Name
+                </label>
+                <input
+                  id="name"
+                  type="text"
+                  placeholder="Your name"
+                  required
+                  onChange={(e) => {
+                    setName(e.target.value);
+                  }}
+                  className="block w-full p-2 border rounded bg-gray-50 dark:bg-gray-700 text-gray-900 dark:text-gray-100 border-gray-300 dark:border-gray-600 focus:ring-blue-500 focus:border-blue-500"
+                />
+              </div>
+              <div className="space-y-2">
+                <label
+                  htmlFor="email"
+                  className="block text-sm font-medium text-gray-900 dark:text-gray-100"
+                >
+                  Email
+                </label>
+                <input
+                  id="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  required
+                  onChange={(e) => {
+                    setEmail(e.target.value);
+                  }}
+                  className="block w-full p-2 border rounded bg-gray-50 dark:bg-gray-700 text-gray-900 dark:text-gray-100 border-gray-300 dark:border-gray-600 focus:ring-blue-500 focus:border-blue-500"
+                />
+              </div>
+              <div className="space-y-2">
+                <label
+                  htmlFor="password"
+                  className="block text-sm font-medium text-gray-900 dark:text-gray-100"
+                >
+                  Password
+                </label>
+                <input
+                  id="password"
+                  type="password"
+                  onChange={(e) => {
+                    setPassword(e.target.value);
+                  }}
+                  placeholder="Create a password"
+                  required
+                  className="block w-full p-2 border rounded bg-gray-50 dark:bg-gray-700 text-gray-900 dark:text-gray-100 border-gray-300 dark:border-gray-600 focus:ring-blue-500 focus:border-blue-500"
+                />
+              </div>
+              <div className="pt-6">
+                <button
+                  onClick={signupHandler}
+                  type="submit"
+                  className="w-full p-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition-colors duration-200"
+                >
+                  Sign Up
+                </button>
+              </div>
+            </form>
+
+            <div className="flex justify-center items-center w-full lg:w-2/3 pt-10 text-sm text-gray-600 dark:text-gray-400">
+              Already have an account?{" "}
+              <Link
+                to="/signin"
+                className="underline text-blue-500 hover:text-blue-600"
+              >
+                Sign in
+              </Link>
+            </div>
+            <div className="absolute bottom-0 left-0 w-full lg:w-2/3  flex justify-center items-center">
+              <div className="">
+                <p className="italic">
+                  "Be yourself; everyone else is already taken."
+                </p>
+                <p className="text-center italic text-sm">― Oscar Wilde </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="hidden h-full lg:block w-full sm:w-1/2">
+          <img src="./writer.svg" alt="writer" className="" />
+        </div>
+      </section>
+      {/* <section className="w-full py-12 md:py-24 lg:py-32">
         <div className="container px-4 md:px-6 grid gap-6 lg:grid-cols-2 lg:gap-12">
           <div className="flex flex-col justify-center space-y-4">
             <div className="space-y-2">
@@ -169,7 +271,7 @@ export default function SignUp() {
             </div>
           </div>
         </div>
-      </section>
+      </section> */}
     </main>
   );
 }
