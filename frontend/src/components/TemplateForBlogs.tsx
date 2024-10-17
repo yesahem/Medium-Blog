ab4a0357f2f294051b9888e42472ba24cbdf3368
vaishvi2811 <vaishvi.khandelwal@gmail.com>
2024-10-17 21:51:55 +0530

    ran npm run build

--- frontend/src/components/TemplateForBlogs.tsx
+++ frontend/src/components/TemplateForBlogs.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useParams, Link, useNavigate } from 'react-router-dom';
 import { format } from 'date-fns';
